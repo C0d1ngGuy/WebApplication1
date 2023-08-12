db9eaaea7c390f74526055654237cccb5f1b6d63
Giovanni Fernandes <giovanni@t2s.com.br>
2023-08-12 00:43:02 -0300

    Start

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -16,12 +16,9 @@ namespace WebApplication1
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
 
             app.UseAuthorization();
 
